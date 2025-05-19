9d227e06d58104a40e682bfe4c447d44f90398ad
Ahmed Karem Mousa <akaremmousa@gmail.com>
2025-05-20 00:00:06 +0300

    chore(format): formatted all files

--- src/connections/mod.rs
+++ src/connections/mod.rs
@@ -1,5 +1,5 @@
 pub mod config;
 pub mod connect;
+pub mod nodes_info;
 pub mod topics;
 pub mod types;
-pub mod nodes_info;
--- src/router/message_router.rs
+++ src/router/message_router.rs
@@ -1,12 +1,7 @@
-
-
-pub struct MessageRouter{}
-pub trait RoutMessage{
+pub struct MessageRouter {}
+pub trait RoutMessage {
     fn send_message();
 }
-impl  RoutMessage for MessageRouter {
-    fn send_message() {
-        
-    }   
+impl RoutMessage for MessageRouter {
+    fn send_message() {}
 }
-
