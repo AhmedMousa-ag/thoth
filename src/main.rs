7ee807188e2895332d7cd6f8763c001493441478
Ahmed Karem Mousa <akaremmousa@gmail.com>
2025-06-15 13:22:44 +0300

    chore: formatted

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use thoth::connections::connect::GossibConnection;
 use thoth::err;
-use thoth::logger::logger::LoggerWritter;
 use thoth::grpc::grpc_server;
+use thoth::logger::logger::LoggerWritter;
 use tokio::spawn;
 #[tokio::main]
 async fn main() {
     LoggerWritter::start().await;
-    spawn(async {grpc_server::start_server().await});
+    spawn(async { grpc_server::start_server().await });
     let conn_res = GossibConnection::p2pconnect().await;
     if conn_res.is_err() {
         err!("Error Establishing Connection: {:?}", conn_res.err());
