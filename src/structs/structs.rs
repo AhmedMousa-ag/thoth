52572f756c7554006c831d85a961f9cce861954c
Ahmed Karem Mousa <akaremmousa@gmail.com>
2025-07-02 17:22:34 +0300

    feat(structs, NodeInfo, Requsts Type): impl Display

--- src/structs/structs.rs
+++ src/structs/structs.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 use bincode::{Decode, Encode};
 
 #[derive(Clone, PartialEq, Debug, Encode, Decode)]
@@ -5,6 +7,17 @@ pub struct NodeInfo {
     pub id: String,
     pub av_threads: usize,
     pub av_ram: u64,
+}
+
+impl fmt::Display for NodeInfo {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Customize so only `x` and `y` are denoted.
+        write!(
+            f,
+            "Id: {}, Available Threads: {}, Available Ram: {}",
+            self.id, self.av_threads, self.av_ram
+        )
+    }
 }
 #[derive(Clone)]
 pub struct Topics {
@@ -21,11 +34,35 @@ pub enum RequestsTypes {
     ReplyNodeInfoUpdate,
     PlansToExecute,
 }
+impl RequestsTypes {
+    pub fn as_str(&self) -> &str {
+        match self {
+            RequestsTypes::RequestNodeInfo => "Request Node Info",
+            RequestsTypes::ReplyNodeInfoUpdate => "Reply Node Info Update",
+            RequestsTypes::PlansToExecute => "Plan to Execute",
+        }
+    }
+}
 
+impl fmt::Display for RequestsTypes {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Request Type: {}, ", self.as_str())
+    }
+}
 #[derive(Debug, Clone, PartialEq, Encode, Decode)]
 pub struct Message {
     // pub parties: MessageParties,
     pub topic_name: String,
     pub request: RequestsTypes,
     pub message: Option<Vec<u8>>,
+}
+
+impl fmt::Display for Message {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "Topic Name: {}, Request Type: {}",
+            self.topic_name, self.request
+        )
+    }
 }
