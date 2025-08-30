f4c24279560a040cd625645393369ee8f0e6f210
Ahmed Karem Mousa <akaremmousa@gmail.com>
2025-08-30 14:26:09 +0300

    debugging

--- src/operations/gatherer/gatherer.rs
+++ src/operations/gatherer/gatherer.rs
@@ -87,7 +87,7 @@ impl Gatherer {
                 return None;
             }
         };
-        debug!("Loaded Gathered Result from DB: {:?}");
+        debug!("Loaded Gathered Result from DB: {:?}",res);
         message.respond = Some(res);
         debug!("Replying Gathered Message: {:?}", message);
         Some(message)
