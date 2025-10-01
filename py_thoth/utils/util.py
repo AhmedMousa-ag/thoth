75ee18eb959d7b0d4c9dae7a527a3fd1fd91bd4a
Ahmed Karem Mousa <akaremmousa@gmail.com>
2025-10-01 23:29:29 +0300

    fix(py_thoth, gRPC): max size

--- py_thoth/utils/util.py
+++ py_thoth/utils/util.py
@@ -12,7 +12,13 @@ def run_client(func):
 
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        with grpc.insecure_channel(random.choice(Config().remote_address)) as channel:
+        conn_options = [
+            ("grpc.max_send_message_length", 1 * 1000 * 1024 * 1024),  # 5GB
+            ("grpc.max_receive_message_length", 1 * 1000 * 1024 * 1024),  # 5GB
+        ]
+        with grpc.insecure_channel(
+            random.choice(Config().remote_address), options=conn_options
+        ) as channel:
             stub = mathop_pb2_grpc.MathOpsStub(channel)
             # Inject the stub into kwargs
             kwargs["stub"] = stub
